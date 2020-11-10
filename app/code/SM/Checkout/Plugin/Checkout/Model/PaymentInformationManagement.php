dba7571f46a5ca472b2412232f87e4f33e9c56c0
Muhammad Randy <rundsoerya@gmail.com>
2020-11-10 11:23:42 +0700

    [UPDATE] 10 Nov 2020 11:23

--- app/code/SM/Checkout/Plugin/Checkout/Model/PaymentInformationManagement.php
+++ app/code/SM/Checkout/Plugin/Checkout/Model/PaymentInformationManagement.php
@@ -16,123 +16,120 @@ namespace SM\Checkout\Plugin\Checkout\Model;
  * Class PaymentInformationManagement
  * @package SM\Checkout\Plugin\Checkout\Model
  */
-class PaymentInformationManagement
-{
-    /**
-     * @var bool
-     */
-    protected $error = false;
+class PaymentInformationManagement {
+	/**
+	 * @var bool
+	 */
+	protected $error = false;
 
-    /**
-     * @var string
-     */
-    protected $message = '';
+	/**
+	 * @var string
+	 */
+	protected $message = '';
 
-    /**
-     * @var \Magento\Quote\Api\CartRepositoryInterface
-     */
-    protected $cartRepository;
+	/**
+	 * @var \Magento\Quote\Api\CartRepositoryInterface
+	 */
+	protected $cartRepository;
 
-    /**
-     * @var \Magento\Checkout\Model\Session
-     */
-    protected $checkoutSession;
+	/**
+	 * @var \Magento\Checkout\Model\Session
+	 */
+	protected $checkoutSession;
 
-    /**
-     * PaymentInformationManagement constructor.
-     * @param \Magento\Quote\Api\CartRepositoryInterface $cartRepository
-     * @param \Magento\Checkout\Model\Session $checkoutSession
-     */
-    public function __construct(
-        \Magento\Quote\Api\CartRepositoryInterface $cartRepository,
-        \Magento\Checkout\Model\Session $checkoutSession
-    ) {
-        $this->cartRepository = $cartRepository;
-        $this->checkoutSession = $checkoutSession;
-    }
+	/**
+	 * PaymentInformationManagement constructor.
+	 * @param \Magento\Quote\Api\CartRepositoryInterface $cartRepository
+	 * @param \Magento\Checkout\Model\Session $checkoutSession
+	 */
+	public function __construct(
+		\Magento\Quote\Api\CartRepositoryInterface $cartRepository,
+		\Magento\Checkout\Model\Session $checkoutSession
+	) {
+		$this->cartRepository  = $cartRepository;
+		$this->checkoutSession = $checkoutSession;
+	}
 
-    /**
-     * @param \Magento\Checkout\Model\PaymentInformationManagement $subject
-     * @param int $cartId
-     * @param \Magento\Quote\Api\Data\PaymentInterface $paymentMethod
-     * @param \Magento\Quote\Api\Data\AddressInterface|null $billingAddress
-     * @return array
-     * @throws \Exception
-     */
-    public function beforeSavePaymentInformationAndPlaceOrder(
-        \Magento\Checkout\Model\PaymentInformationManagement $subject,
-        $cartId,
-        \Magento\Quote\Api\Data\PaymentInterface $paymentMethod,
-        \Magento\Quote\Api\Data\AddressInterface $billingAddress = null
-    ) {
-        $this->checkoutSession->setArea(\SM\Checkout\Helper\OrderReferenceNumber::AREA_WEB);
-        $quote = $this->cartRepository->get($cartId);
-        if (!$quote->getIsVirtual()) {
-            $items = $this->handleQuoteAddress($quote);
-            if (!$this->error) {
-                foreach ($quote->getAllVisibleItems() as $item) {
-                    $itemId = $item->getId();
-                    $qty = (int)$item->getQty();
-                    if (!isset($items[$itemId]) || $items[$itemId] != $qty) {
-                        $this->error = true;
-                        $this->message = __('Cart has been updated.');
-                        break;
-                    }
-                }
-            }
+	/**
+	 * @param \Magento\Checkout\Model\PaymentInformationManagement $subject
+	 * @param int $cartId
+	 * @param \Magento\Quote\Api\Data\PaymentInterface $paymentMethod
+	 * @param \Magento\Quote\Api\Data\AddressInterface|null $billingAddress
+	 * @return array
+	 * @throws \Exception
+	 */
+	public function beforeSavePaymentInformationAndPlaceOrder(
+		\Magento\Checkout\Model\PaymentInformationManagement $subject,
+		$cartId,
+		\Magento\Quote\Api\Data\PaymentInterface $paymentMethod,
+		\Magento\Quote\Api\Data\AddressInterface $billingAddress = null
+	) {
+		$this->checkoutSession->setArea(\SM\Checkout\Helper\OrderReferenceNumber::AREA_WEB);
+		$quote = $this->cartRepository->get($cartId);
+		if (!$quote->getIsVirtual()) {
+			$items = $this->handleQuoteAddress($quote);
+			if (!$this->error) {
+				foreach ($quote->getAllVisibleItems() as $item) {
+					$itemId = $item->getId();
+					$qty    = (int) $item->getQty();
+					if (!isset($items[$itemId]) || $items[$itemId] != $qty) {
+						$this->error   = true;
+						$this->message = __('Cart has been updated.');
+						break;
+					}
+				}
+			}
 
-            if ($this->error) {
-                throw new \Exception($this->message);
-            }
-        }
-        return [$cartId, $paymentMethod, $billingAddress];
-    }
+			if ($this->error) {
+				throw new \Exception($this->message);
+			}
+		}
+		return [$cartId, $paymentMethod, $billingAddress];
+	}
 
-    /**
-     * @param $quote
-     * @return array
-     */
-    protected function handleQuoteAddress($quote)
-    {
-        $items = [];
-        foreach ($quote->getAllShippingAddresses() as $_address) {
-            $shippingMethodList = [];
-            $_shippingRateGroups = $_address->getGroupedAllShippingRates();
-            if ($_shippingRateGroups) {
-                foreach ($_shippingRateGroups as $code => $_rates) {
-                    foreach ($_rates as $_rate) {
-                        $shippingMethodList[] = $_rate->getCode();
-                        if (!in_array($_rate->getCode(), $shippingMethodList)) {
-                            $shippingMethodList[] = $_rate->getCode();
-                        }
-                    }
-                }
-            }
-            if (!in_array($_address->getShippingMethod(), $shippingMethodList)) {
-                $this->error = true;
-                $this->message = __('OAR: Invalid Shipping Method.');
-                break;
-            }
-            $items = $items + $this->getItemsOfAddress($_address);
-        }
-        return $items;
-    }
+	/**
+	 * @param $quote
+	 * @return array
+	 */
+	protected function handleQuoteAddress($quote) {
+		$items = [];
+		foreach ($quote->getAllShippingAddresses() as $_address) {
+			$shippingMethodList  = [];
+			$_shippingRateGroups = $_address->getGroupedAllShippingRates();
+			if ($_shippingRateGroups) {
+				foreach ($_shippingRateGroups as $code => $_rates) {
+					foreach ($_rates as $_rate) {
+						$shippingMethodList[] = $_rate->getCode();
+						if (!in_array($_rate->getCode(), $shippingMethodList)) {
+							$shippingMethodList[] = $_rate->getCode();
+						}
+					}
+				}
+			}
+			// if (!in_array($_address->getShippingMethod(), $shippingMethodList)) {
+			//     $this->error = true;
+			//     $this->message = __('OAR: Invalid Shipping Method.');
+			//     break;
+			// }
+			$items = $items + $this->getItemsOfAddress($_address);
+		}
+		return $items;
+	}
 
-    /**
-     * @param $address
-     * @return array
-     */
-    protected function getItemsOfAddress($address)
-    {
-        $items = [];
-        foreach ($address->getAllVisibleItems() as $item) {
-            if ($item instanceof \Magento\Quote\Model\Quote\Address\Item) {
-                $items[$item->getQuoteItemId()] = (int)$item->getQty();
-            } else {
-                $items[$item->getId()] = (int)$item->getQty();
-            }
+	/**
+	 * @param $address
+	 * @return array
+	 */
+	protected function getItemsOfAddress($address) {
+		$items = [];
+		foreach ($address->getAllVisibleItems() as $item) {
+			if ($item instanceof \Magento\Quote\Model\Quote\Address\Item) {
+				$items[$item->getQuoteItemId()] = (int) $item->getQty();
+			} else {
+				$items[$item->getId()] = (int) $item->getQty();
+			}
 
-        }
-        return $items;
-    }
+		}
+		return $items;
+	}
 }
