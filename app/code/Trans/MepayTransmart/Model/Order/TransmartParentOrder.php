a855520ed0d017e227bbd2df8f571e6c187a44b6
anan.fauzi <anan.fauzi@transdigital.co.id>
2020-11-26 16:03:22 +0700

    Add see details information for Bank Mega Payment

--- app/code/Trans/MepayTransmart/Model/Order/TransmartParentOrder.php
+++ app/code/Trans/MepayTransmart/Model/Order/TransmartParentOrder.php
@@ -206,7 +206,7 @@ class TransmartParentOrder extends ParentOrder
     {
         if (!isset($this->howToPay[$method])) {
             try {
-                $howToPay = $this->paymentHelper->getBlockHowToPay($method, true);
+                $howToPay = (Data::isMegaMethod($method))? $this->paymentHelper->getBlockHowToPay('', true) : $this->paymentHelper->getBlockHowToPay($method, true);
             } catch (LocalizedException $e) {
                 $this->logger->critical($e->getMessage());
                 $howToPay = null;
