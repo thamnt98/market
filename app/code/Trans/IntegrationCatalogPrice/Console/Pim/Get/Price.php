d2fa5426fa650a5dce22b84b455d213de64c80a8
anan.fauzi <anan.fauzi@transdigital.co.id>
2020-12-07 10:32:51 +0700

    Add missing class

---
+++ app/code/Trans/IntegrationCatalogPrice/Console/Pim/Get/Price.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * @category Trans
+ * @package  Trans_IntegrationCatalogPrice
+ * @license  http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *
+ *
+ * @author   Anan Fauzi <anan.fauzi@transdigital.co.id>
+ *
+ * Copyright © 2019 PT Trans Digital. All rights reserved.
+ * http://www.ctcorpora.com
+ */
+namespace Trans\IntegrationCatalogPrice\Console\Pim\Get;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Trans\IntegrationCatalogPrice\Cron\Pim\Get\Price as cronPrice;
+use Magento\Framework\App\State;
+
+class Price extends Command
+{
+    /**
+     * @var \Trans\IntegrationCatalogPrice\Cron\Pim\Get\Price
+     */
+    protected $cronPrice;
+
+    /**
+     * @var \Magento\Framework\App\State
+     */
+    protected $state;
+    /**
+     * Constructor method
+     * @param cronPrice $cronPrice
+     * @param State $state
+     */
+    public function __construct(
+        cronPrice $cronPrice,
+        State $state
+    ) {
+        $this->cronPrice = $cronPrice;
+        $this->state = $state;
+        parent::__construct();
+    }
+
+    /**
+     * Console configure
+     * @return void
+     */
+    protected function configure()
+    {
+        $this->setName('integration:pim-get:price');
+        $this->setDescription('Get pim Product price integration');
+        parent::configure();
+    }
+
+    /**
+        * Console execution
+        * @param  InputInterface  $input
+        * @param  OutputInterface $output
+        * @return void
+        * @throw \Exception
+        */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->state->setAreaCode(\Magento\Framework\App\Area::AREA_GLOBAL);
+        $this->cronPrice->execute();
+    }
+}
