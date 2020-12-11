fac8e466a15de421ad362c666e55516049ced3a2
Nam nguyen <namnd2@smartosc.com>
2020-12-11 17:14:11 +0700

    APO-5954: [Web][FUNC][Fresh Product] Fresh products should display correct format on Checkout page

--- app/code/SM/Checkout/view/frontend/web/js/view/cart-items/details.js
+++ app/code/SM/Checkout/view/frontend/web/js/view/cart-items/details.js
@@ -97,7 +97,7 @@ define([
 
         isFreshProduct: function (item_id) {
             if (imageData[item_id]) {
-                if (imageData[item_id].own_courier == 1) {
+                if (imageData[item_id].is_fresh == 1) {
                     return true
                 }
             }
