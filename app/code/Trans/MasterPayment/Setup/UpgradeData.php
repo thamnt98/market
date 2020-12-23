a9a87109bc8181846a173cdd90c365af0f34ad4f
randy.surya <muhammad.randy@transdigital.co.id>
2020-12-23 15:02:30 +0700

    [UPDATE] added mepay debit number to oms in masterpayment module

--- app/code/Trans/MasterPayment/Setup/UpgradeData.php
+++ app/code/Trans/MasterPayment/Setup/UpgradeData.php
@@ -17,35 +17,44 @@ use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\UpgradeDataInterface;
 
-class UpgradeData implements UpgradeDataInterface
-{
+class UpgradeData implements UpgradeDataInterface {
 
-    /**
-     * Upgrade DB schema for a module
-     *
-     * @param ModuleDataSetupInterface $setup
-     * @param ModuleContextInterface $context
-     * @return void
-     */
-    public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
-    {
-        $installer = $setup;
-        $installer->startSetup();
+	/**
+	 * Upgrade DB schema for a module
+	 *
+	 * @param ModuleDataSetupInterface $setup
+	 * @param ModuleContextInterface $context
+	 * @return void
+	 */
+	public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context) {
+		$installer = $setup;
+		$installer->startSetup();
 
-        $masterPaymentTable = $setup->getTable('master_payment');
-        if (version_compare($context->getVersion(), '0.0.3', '<')) {
-            if ($setup->getConnection()->isTableExists($masterPaymentTable) == true) {
-                $data = [
-                    ["1010000024", "Credit Card Bank Mega", "sprint_mega_cc", ""],
-                    ["1011000024", "Debit Card Bank Mega", "sprint_mega_debit", ""],
-                    ["1008000024", "Bank Mega VA", "trans_mepay_va", ""],
-                    ["10100000241", "Credit and Debit Card Bank Mega (Bank Mega PG)", "trans_mepay_cc", ""],
-                    ["1016000024", "QR Code Bank Mega", "trans_mepay_qris", ""],
-                ];
-                $columns = ['payment_id', 'payment_title', 'payment_method', 'payment_terms'];
-                $setup->getConnection()->insertArray($masterPaymentTable, $columns, $data);
-            }
-            $setup->endSetup();
-        }
-    }
+		$masterPaymentTable = $setup->getTable('master_payment');
+		if (version_compare($context->getVersion(), '0.0.3', '<')) {
+			if ($setup->getConnection()->isTableExists($masterPaymentTable) == true) {
+				$data = [
+					["1010000024", "Credit Card Bank Mega", "sprint_mega_cc", ""],
+					["1011000024", "Debit Card Bank Mega", "sprint_mega_debit", ""],
+					["1008000024", "Bank Mega VA", "trans_mepay_va", ""],
+					["10100000241", "Credit and Debit Card Bank Mega (Bank Mega PG)", "trans_mepay_cc", ""],
+					["1016000024", "QR Code Bank Mega", "trans_mepay_qris", ""],
+				];
+				$columns = ['payment_id', 'payment_title', 'payment_method', 'payment_terms'];
+				$setup->getConnection()->insertArray($masterPaymentTable, $columns, $data);
+			}
+			$setup->endSetup();
+		}
+
+		if (version_compare($context->getVersion(), '0.0.4', '<')) {
+			if ($setup->getConnection()->isTableExists($masterPaymentTable) == true) {
+				$data = [
+					["10110000241", "Debit Card Bank Mega (Bank Mega PG)", "trans_mepay_debit", ""],
+				];
+				$columns = ['payment_id', 'payment_title', 'payment_method', 'payment_terms'];
+				$setup->getConnection()->insertArray($masterPaymentTable, $columns, $data);
+			}
+			$setup->endSetup();
+		}
+	}
 }
