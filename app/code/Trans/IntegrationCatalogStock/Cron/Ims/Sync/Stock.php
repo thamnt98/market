1066f4510a28c83d8f84f9d259c822ae178b3bfb
ITOfJob <itofjob@gmail.com>
2021-01-13 07:16:36 +0700

    modifying sync-stock-cron

--- app/code/Trans/IntegrationCatalogStock/Cron/Ims/Sync/Stock.php
+++ app/code/Trans/IntegrationCatalogStock/Cron/Ims/Sync/Stock.php
@@ -14,84 +14,29 @@
 
 namespace Trans\IntegrationCatalogStock\Cron\Ims\Sync;
 
+use Magento\InventoryIndexer\Indexer\SourceItem\IndexDataBySkuListProvider;
+use Magento\Framework\App\ResourceConnection;
+use Magento\Framework\Stdlib\DateTime\TimezoneInterface;
+
+use Trans\Integration\Model\IntegrationCronLogToDatabase;
+use Trans\IntegrationCatalogStock\Logger\Logger;
 
 use Trans\Integration\Exception\WarningException;
 use Trans\Integration\Exception\ErrorException;
 use Trans\Integration\Exception\FatalException;
 
+use Trans\Integration\Utility\CurlError;
+
 
 class Stock {
-
-    public const CURL_ERRNO = [
-        1  => "CURL_UNSUPPORTED_PROTOCOL",
-        2  => "CURL_FAILED_INIT",
-        3  => "CURL_URL_MALFORMAT",
-        4  => "CURL_URL_MALFORMAT_USER",
-        5  => "CURL_COULDNT_RESOLVE_PROXY",
-        6  => "CURL_COULDNT_RESOLVE_HOST",
-        7  => "CURL_COULDNT_CONNECT",
-        8  => "CURL_FTP_WEIRD_SERVER_REPLY",
-        9  => "CURL_FTP_ACCESS_DENIED",
-        10 => "CURL_FTP_USER_PASSWORD_INCORRECT",
-        11 => "CURL_FTP_WEIRD_PASS_REPLY",
-        12 => "CURL_FTP_WEIRD_USER_REPLY",
-        13 => "CURL_FTP_WEIRD_PASV_REPLY",
-        14 => "CURL_FTP_WEIRD_227_FORMAT",
-        15 => "CURL_FTP_CANT_GET_HOST",
-        16 => "CURL_FTP_CANT_RECONNECT",
-        17 => "CURL_FTP_COULDNT_SET_BINARY",
-        18 => "CURL_FTP_PARTIAL_FILE or CURL_PARTIAL_FILE",
-        19 => "CURL_FTP_COULDNT_RETR_FILE",
-        20 => "CURL_FTP_WRITE_ERROR",
-        21 => "CURL_FTP_QUOTE_ERROR",
-        22 => "CURL_HTTP_NOT_FOUND or CURL_HTTP_RETURNED_ERROR",
-        23 => "CURL_WRITE_ERROR",
-        24 => "CURL_MALFORMAT_USER",
-        25 => "CURL_FTP_COULDNT_STOR_FILE",
-        26 => "CURL_READ_ERROR",
-        27 => "CURL_OUT_OF_MEMORY",
-        28 => "CURL_OPERATION_TIMEDOUT or CURL_OPERATION_TIMEOUTED",
-        29 => "CURL_FTP_COULDNT_SET_ASCII",
-        30 => "CURL_FTP_PORT_FAILED",
-        31 => "CURL_FTP_COULDNT_USE_REST",
-        32 => "CURL_FTP_COULDNT_GET_SIZE",
-        33 => "CURL_HTTP_RANGE_ERROR",
-        34 => "CURL_HTTP_POST_ERROR",
-        35 => "CURL_SSL_CONNECT_ERROR",
-        36 => "CURL_BAD_DOWNLOAD_RESUME or CURL_FTP_BAD_DOWNLOAD_RESUME",
-        37 => "CURL_FILE_COULDNT_READ_FILE",
-        38 => "CURL_LDAP_CANNOT_BIND",
-        39 => "CURL_LDAP_SEARCH_FAILED",
-        40 => "CURL_LIBRARY_NOT_FOUND",
-        41 => "CURL_FUNCTION_NOT_FOUND",
-        42 => "CURL_ABORTED_BY_CALLBACK",
-        43 => "CURL_BAD_FUNCTION_ARGUMENT",
-        44 => "CURL_BAD_CALLING_ORDER",
-        45 => "CURL_HTTP_PORT_FAILED",
-        46 => "CURL_BAD_PASSWORD_ENTERED",
-        47 => "CURL_TOO_MANY_REDIRECTS",
-        48 => "CURL_UNKNOWN_TELNET_OPTION",
-        49 => "CURL_TELNET_OPTION_SYNTAX",
-        50 => "CURL_OBSOLETE",
-        51 => "CURL_SSL_PEER_CERTIFICATE",
-        52 => "CURL_GOT_NOTHING",
-        53 => "CURL_SSL_ENGINE_NOTFOUND",
-        54 => "CURL_SSL_ENGINE_SETFAILED",
-        55 => "CURL_SEND_ERROR",
-        56 => "CURL_RECV_ERROR",
-        57 => "CURL_SHARE_IN_USE",
-        58 => "CURL_SSL_CERTPROBLEM",
-        59 => "CURL_SSL_CIPHER",
-        60 => "CURL_SSL_CACERT",
-        61 => "CURL_BAD_CONTENT_ENCODING",
-        62 => "CURL_LDAP_INVALID_URL",
-        63 => "CURL_FILESIZE_EXCEEDED",
-        64 => "CURL_FTP_SSL_FAILED",
-        79 => "CURL_SSH"
-    ];
-    
+	
+	/**
+	 * @var \Magento\InventoryIndexer\Indexer\SourceItem\IndexDataBySkuListProvider
+	 */
+	protected $indexDataBySkuListProvider;
+	
     /**
-     * @var ResourceConnection
+     * @var \Magento\Framework\App\ResourceConnection
      */
 	protected $dbConnection;
 
@@ -118,19 +63,26 @@ class Stock {
     /**
      * @var string
      */
-    protected $cronTypeDetail;    
+	protected $cronTypeDetail;
+	
+    /**
+     * @var string
+     */
+    protected $cronFileLabel;
 
 
     public function __construct(
-        \Magento\Framework\App\ResourceConnection $resourceConnection,
-        \Magento\Framework\Stdlib\DateTime\TimezoneInterface $timezone,
-        \Trans\IntegrationCatalogStock\Logger\Logger $loggerfile,
-        \Trans\Integration\Model\IntegrationCronLogToDatabase $loggerdb
+		IndexDataBySkuListProvider $indexDataBySkuListProvider,
+        ResourceConnection $resourceConnection,
+		TimezoneInterface $timezone,
+        Logger $loggerfile,
+		IntegrationCronLogToDatabase $loggerdb
     ) {
-        $this->dbConnection = $resourceConnection->getConnection();
-        $this->timezone = $timezone;
+		$this->indexDataBySkuListProvider = $indexDataBySkuListProvider;
+		$this->dbConnection = $resourceConnection->getConnection();
+		$this->timezone = $timezone;
         $this->loggerfile = $loggerfile;
-        $this->loggerdb = $loggerdb;
+		$this->loggerdb = $loggerdb;		
 
         $this->adjustCronLogger();
     }
@@ -150,7 +102,7 @@ class Stock {
 
         $tagChannel = 'v2-product-stock-update';        
         
-        $apiTimeout = 60;
+        $apiTimeout = 5;
         $apiCallDuration = -1;
 
 		$limitDataToApi = 0;
@@ -185,19 +137,23 @@ class Stock {
 
 			$sql = "select `last_id` from `v2_monitoring_stock_last_retrieved` limit 1 for update";
 			$meta['last_id'] = $this->dbConnection->fetchOne($sql);
+			
 			if (empty($meta['last_id'])) {
 				$meta['last_id'] = "";
 			}
+			
 			$this->loggerfile->info($this->cronFileLabel . "last-retrieved-stock-id = '" . $meta['last_id'] . "'");
 		
 			$sql = "select `ch_id`, `headers`, `query_params`, `path`, `limit` from `integration_channel_method` where `tag` = '" . $tagChannel . "' and `status` = 1 limit 1";
 			$meta['method'] = $this->dbConnection->fetchRow($sql);    
+			
 			if (empty($meta['method'])) {
 				throw new ErrorException('integration-channel-method not found');
 			}
 				
 			$sql = "select `url` from `integration_channel` where `id` = " . $meta['method']['ch_id'] . " limit 1";
 			$meta['channel'] = $this->dbConnection->fetchRow($sql);    
+			
 			if (empty($meta['channel'])) {
 				throw new ErrorException('integration-channel not found');
 			}
@@ -208,6 +164,7 @@ class Stock {
 			$sql = "select (select `attribute_id` from `eav_attribute` where `attribute_code` = 'is_fresh' and `frontend_input` = 'boolean' and `entity_type_id` = e.`entity_type_id`) as `is_fresh`, (select `attribute_id` from `eav_attribute` where `attribute_code` = 'sold_in' and `entity_type_id` = e.`entity_type_id`) as `sold_in`, (select `attribute_id` from `eav_attribute` where `attribute_code` = 'weight' and `entity_type_id` = e.`entity_type_id`) as `weight` from `eav_entity_type` e where `entity_type_code` = 'catalog_product' limit 1";
 			$attr = $this->dbConnection->fetchRow($sql);
 			unset($sql);
+			
 			if (empty($attr)) {
 				throw new ErrorException('attribute-product not found');
 			}
@@ -233,13 +190,13 @@ class Stock {
 			curl_setopt($curlHolder, CURLOPT_TIMEOUT, $apiTimeout);
 			$curlOutput = curl_exec($curlHolder);
 			$curlInfo = curl_getinfo($curlHolder);
-			$curlErrNo = curl_errno($curlHolder);
+			$curlErrno = curl_errno($curlHolder);
 			curl_close($curlHolder);
 			
 			$apiCallDuration = $curlInfo['total_time'];
 			
-			if (!empty($curlErrNo)) {
-				throw new ErrorException("api-call error = " . $this::CURL_ERRNO[(int) $curlErrNo]);
+			if (!empty($curlErrno)) {
+				throw new ErrorException("api-call error = " . CurlError::ERROR[(int) $curlErrno]);
 			}    
 			
 			$curlResponse = json_decode($curlOutput, true);
@@ -369,7 +326,9 @@ class Stock {
                 unset($sql);
 
 				if (!empty($collections)) {
+
 					foreach ($collections as $item) {
+
 						if (!isset($stockCandidatePointerList[$item['sku']])) {
 							$item['sku'] = strtoupper($item['sku']);
 							if (!isset($stockCandidatePointerList[$item['sku']])) {
@@ -378,7 +337,8 @@ class Stock {
 									continue;
 								}
 							}
-						}                
+						}
+
 						foreach ($stockCandidatePointerList[$item['sku']] as $idx) {
 							if ($item['is_fresh'] == 1) {
 								if ($item['sold_in'] == 'kg' || $item['sold_in'] == 'Kg' || $item['sold_in'] == 'KG') {
@@ -393,7 +353,9 @@ class Stock {
 								}
 							}	
 						}
+
 					}
+
 				}        
 		
 				$mainSql = "";
@@ -523,7 +485,11 @@ class Stock {
 			$sql = "set session innodb_lock_wait_timeout = @saved_lock_wait";
             $this->dbConnection->exec($sql);
 			
-            $this->dbConnection->commit();
+			$this->dbConnection->commit();
+			
+			$this->indexDataBySkuListProvider->execute(1, $skuList);
+			$this->indexDataBySkuListProvider->execute(2, $skuList);
+			unset($skuList);
 
 		}
 		catch (WarningException $ex) {       
@@ -573,7 +539,7 @@ class Stock {
                 }
 
                 try {					
-                    $sql = "insert ignore into `v2_monitoring_stock_job` (`id`, `memory_usage_megabytes`, `process_duration_seconds`, `api_call_duration_seconds`, `limit_data_to_api`, `total_data_from_api_received`, `total_data_from_api_received_valid`, `total_data_from_api_received_invalid`, `total_data_from_api_saved`, `total_data_from_api_updated_to_magento_stock`, `status`, `message`) values (sysdate(6), " . round(memory_get_usage() / 1048576, 2) . "," . (microtime(true) - $startTime) . ", {$apiCallDuration}, {$limitDataToApi}, {$totalDataFromApiReceived}, " . ($totalDataFromApiReceived - $totalDataFromApiReceivedInvalid) . ", {$totalDataFromApiReceivedInvalid}, {$totalDataFromApiSaved}, {$totalDataFromApiUpdatedToMagentoStock}, '{$logMessageTopic}', '" . addslashes($ex->getMessage()) . "')";
+                    $sql = "insert ignore into `v2_monitoring_stock_job` (`id`, `memory_usage_megabytes`, `process_duration_seconds`, `api_call_duration_seconds`, `limit_data_to_api`, `total_data_from_api_received`, `total_data_from_api_received_valid`, `total_data_from_api_received_invalid`, `total_data_from_api_saved`, `total_data_from_api_updated_to_magento_stock`, `status`, `message`) values (sysdate(6), " . round(memory_get_usage() / 1048576, 2) . "," . (microtime(true) - $startTime) . ", {$apiCallDuration}, {$limitDataToApi}, {$totalDataFromApiReceived}, " . ($totalDataFromApiReceived - $totalDataFromApiReceivedInvalid) . ", {$totalDataFromApiReceivedInvalid}, {$totalDataFromApiSaved}, {$totalDataFromApiUpdatedToMagentoStock}, '{$logMessageTopic}', '" . addslashes($logMessage) . "')";
                     $res = $this->dbConnection->exec($sql);
                     
                     $this->loggerfile->info($this->cronFileLabel . "insert-ignore monitoring-stock-job result = " . $res);
