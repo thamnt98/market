3d8344f8bd9e96cbef2b6d871b3c458bff9d49b3
Phạm Văn Minh <minhpv2@smartosc.com>
2021-02-25 15:37:38 +0700

    APO-6517: Installation For Supplier

--- app/code/SM/Sales/Model/OrderInstallation.php
+++ app/code/SM/Sales/Model/OrderInstallation.php
@@ -5,13 +5,18 @@ namespace SM\Sales\Model;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\Mail\Template\SenderResolverInterface;
 use Magento\Sales\Model\Order\Address\Renderer;
+use Magento\Store\Model\ScopeInterface;
+use Magento\Framework\Serialize\SerializerInterface;
 use SM\Email\Model\Email\Sender as EmailSender;
 
 class OrderInstallation
 {
     const EMAIL_TEMPLATE_ORDER_INSTALLATION = 'installation/email/template';
+    const EMAIL_TEMPLATE_ORDER_INSTALLATION_SUPPLIER = 'installation/supplier_email/template';
     const EMAIL_SENDER_ORDER_INSTALLATION = 'installation/email/sender';
+    const EMAIL_SENDER_ORDER_INSTALLATION_SUPPLIER = 'installation/supplier_email/sender';
     const EMAIL_RECEIVER_ORDER_INSTALLATION = 'installation/email/receiver';
+    const XPATH_MAPPING_VARIABLES = 'installation/supplier_email/variables';
 
     /**
      * @var EmailSender
@@ -32,22 +37,29 @@ class OrderInstallation
      * @var SenderResolverInterface
      */
     protected $senderResolver;
+    /**
+     * @var SerializerInterface
+     */
+    private $serializer;
 
     /**
      * OrderInstallation constructor.
      * @param ScopeConfigInterface $scopeConfig
+     * @param SerializerInterface $serializer
      * @param Renderer $addressRenderer
      * @param EmailSender $emailSender
      * @param SenderResolverInterface $senderResolver
      */
     public function __construct(
         ScopeConfigInterface $scopeConfig,
+        SerializerInterface $serializer,
         Renderer $addressRenderer,
         EmailSender $emailSender,
         SenderResolverInterface $senderResolver
     ) {
         $this->addressRenderer = $addressRenderer;
         $this->scopeConfig = $scopeConfig;
+        $this->serializer = $serializer;
         $this->emailSender = $emailSender;
         $this->senderResolver = $senderResolver;
     }
@@ -60,12 +72,37 @@ class OrderInstallation
     public function sendMail($order)
     {
         $isInstallation = false;
+        $mappingVariables = $this->getMappingVariables();
         foreach ($order->getItems() as $item) {
             $buyRequest = $item->getProductOptionByCode('info_buyRequest');
             if (isset($buyRequest['installation_service'])) {
                 if ($buyRequest['installation_service']['is_installation'] == 1) {
                     $isInstallation = true;
-                    break;
+                    if (in_array($item->getSku(), [])) {
+                        $templateId = $this->getConfigEmailID(self::EMAIL_TEMPLATE_ORDER_INSTALLATION_SUPPLIER);
+                        $storeId = (int)$order->getStoreId();
+                        $sender = $this->getConfigEmailID(self::EMAIL_SENDER_ORDER_INSTALLATION_SUPPLIER);
+                        $email = $this->getConfigEmailID(self::EMAIL_RECEIVER_ORDER_INSTALLATION);
+                        $name = $order->getCustomerLastname();
+                        $templateVars = [
+                            "customer_name" => $order->getId(),
+                            "phone_number" => $order->getId(),
+                            "address" => $order->getId(),
+                            "product" => $order->getId(),
+                            "qty" => $order->getId(),
+                            "order_id" => $order->getId(),
+                            "estimate_date_of_delivery" => $order->getId(),
+                            "estimate_date_of_receive" => $order->getId()
+                        ];
+                        $this->emailSender->sends(
+                            $templateId,
+                            $sender,
+                            $email,
+                            $name,
+                            $templateVars,
+                            $storeId
+                        );
+                    }
                 }
             }
         }
@@ -116,5 +153,21 @@ class OrderInstallation
         return $order->getIsVirtual()
             ? null
             : $this->addressRenderer->format($order->getShippingAddress(), 'html');
+    }
+
+    /**
+     * @param string $scopeType
+     * @param string|int|null $scopeCode
+     * @return array
+     */
+    public function getMappingVariables($scopeType = ScopeInterface::SCOPE_STORE, $scopeCode = null)
+    {
+        $mappingVariables = $this->scopeConfig->getValue(
+            self::XPATH_MAPPING_VARIABLES,
+            $scopeType,
+            $scopeCode
+        );
+
+        return $this->serializer->unserialize($mappingVariables) ?: [];
     }
 }
